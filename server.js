a9ecd556b072346ce4cb63e319e8bb834a15a447
Mattia Collalti <collalti.mattia@gmail.com>
2023-02-27 16:56:01 +0100

    initial deploy

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, 'static')))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/home', (req, res) => {
+    res.render('pages/index', { pageTitle: "Platone" });
 })
+
+app.use(express.static(path.join(__dirname, 'static')))
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
