e49e9607b63024a44298a9269f6faf7108f6d4e9
Mattia Collalti <collalti.mattia@gmail.com>
2023-02-27 12:16:03 +0000

    start implementing

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.use(express.static(path.join()))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, , 'static', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+})
