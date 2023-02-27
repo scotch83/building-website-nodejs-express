442744e079891a128172d77e17fd17a1b7c5150d
Mattia Collalti <collalti.mattia@gmail.com>
2023-02-27 12:53:35 +0000

    start committing server

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.use(express.static(path.join()))
+app.use(express.static(path.join(__dirname, 'static')))
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, , 'static', 'index.html'))
+    res.sendFile(path.join(__dirname, 'static', 'index.html'))
 })
 
 app.listen(port, () => {
